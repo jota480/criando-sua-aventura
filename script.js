3033ed069f305d8877b352909ae98f1fac805132
jota480 <jose.binde@escola.pr.gov.br>
2025-09-30 09:22:54 -0300

    script

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-const avanca = document,querySelectorAll(",")
+const avanca = document.querySelectorAll(".btn-proximo")
+
+avança.forEach(button => {
+  button.addEventListener('click',function()){
+     const atual = document.querySelector('.ativo');
+     const proximopasso = 'passo-' +this .getAttribute('data-proximo');
+  
+     atual .classList.remove('ativo');
+     document.getElementById(proximopasso).classList.add('ativo')
+    
+    })    
+})
